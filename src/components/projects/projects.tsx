b2861a6a9e431338486222fb7ac76e55d2345a17
Daniel Domenech Moreno <71819787+ddm19@users.noreply.github.com>
2025-08-20 14:31:53 +0200

    feat: apply translations to about and projects

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -6,12 +6,14 @@ import { projects, technologies } from './constants';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faContactCard, faFileDownload } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { useTranslation } from 'react-i18next';
 
 const Projects = () => {
+    const { t } = useTranslation();
 
     return (
         <section className="projects customSection" id="projects">
-            <h2>Portfolio</h2>
+            <h2>{t('projects.title')}</h2>
 
             <div className="projects__content">
 
@@ -26,15 +28,15 @@ const Projects = () => {
 
                 ))} autoPlayDefault={false} interval={3000} itemsPerPage={3} autoPlayDelay={5000} />
             </div>
-            <h2>Te gusta lo que ves?</h2>
-            <p className='projects__stack'>Este es mi stack principal</p>
+            <h2>{t('projects.like')}</h2>
+            <p className='projects__stack'>{t('projects.stack')}</p>
             <TechnologyList technologies={technologies} />
-            <h2>Hablemos!</h2>
+            <h2>{t('projects.talk')}</h2>
             <div className="projects__buttons">
-                <a href='#contact'><button><FontAwesomeIcon icon={faContactCard} /> Contáctame</button></a>
-                <a href="/CV 062025.pdf" download><button ><FontAwesomeIcon icon={faFileDownload} /> Descargar CV</button></a>
+                <a href='#contact'><button><FontAwesomeIcon icon={faContactCard} /> {t('projects.contact')}</button></a>
+                <a href="/CV 062025.pdf" download><button ><FontAwesomeIcon icon={faFileDownload} /> {t('projects.download')}</button></a>
                 <a href="https://github.com/ddm19" target="_blank" rel="noopener noreferrer">
-                    <button><FontAwesomeIcon icon={faGithub} /> Ver más proyectos</button>
+                    <button><FontAwesomeIcon icon={faGithub} /> {t('projects.more')}</button>
                 </a>
             </div>
         </section>
